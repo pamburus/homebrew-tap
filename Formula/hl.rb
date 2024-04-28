cf8350c5ba7b56569887ad5e1660720399032e39
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-04-28 03:21:02 +0200

    fix: hl: simplified description
    Closes #2.
    
    Signed-off-by: Pavel Ivanov <12778560+pamburus@users.noreply.github.com>

--- Formula/hl.rb
+++ Formula/hl.rb
@@ -1,5 +1,5 @@
 class Hl < Formula
-  desc "Log viewer and processor for JSON and logfmt logs"
+  desc "Log viewer for JSON and logfmt logs"
   homepage "https://github.com/pamburus/hl"
   url "https://github.com/pamburus/hl/archive/refs/tags/v0.27.4.tar.gz"
   sha256 "f1a6420d7fb0d049ddbd39e6844afe33c625f7a43816457219c4a8efad5c4c7c"
