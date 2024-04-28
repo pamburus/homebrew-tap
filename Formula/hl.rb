bbc6a0802fd7405677fc26b12f0a0090b7114a5c
Pavel Ivanov <12778560+pamburus@users.noreply.github.com>
2024-04-28 09:52:28 +0000

    hl: add 0.27.4 bottle.

--- Formula/hl.rb
+++ Formula/hl.rb
@@ -6,6 +6,13 @@ class Hl < Formula
   license "MIT"
   head "https://github.com/pamburus/hl.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.27.4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:      "6cc299de96ccbb29e981e26e1d4a887453e6d7a80ca1e34bfda300ac51a6a108"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c10f29eea1ade7fd418223a9e9681c3fbaf738c11aa172a9b5fe13d610ca5cb0"
+  end
+
   depends_on "rust" => :build
 
   def install
