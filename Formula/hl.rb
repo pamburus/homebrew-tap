51cc9f3631fe3b1e65ac153d1bb4266fa89eebc0
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-04-28 18:01:42 +0200

    new: updated hl to v0.28.0
    Closes #6.
    
    Signed-off-by: Pavel Ivanov <12778560+pamburus@users.noreply.github.com>

--- Formula/hl.rb
+++ Formula/hl.rb
@@ -1,17 +1,10 @@
 class Hl < Formula
   desc "Log viewer for JSON and logfmt logs"
   homepage "https://github.com/pamburus/hl"
-  url "https://github.com/pamburus/hl/archive/refs/tags/v0.28.0-beta.1.tar.gz"
-  sha256 "46a420bcf62f5bdbf327e3b8345504595b46cb0747e9ae6d819f013d3cc90d34"
+  url "https://github.com/pamburus/hl/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "63d6c96b0cf7e60514acec3a36d025a4e429dea6b0f8a2d7d1af0b9c87156ff9"
   license "MIT"
   head "https://github.com/pamburus/hl.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.28.0-beta.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "a76aea1aebcfb4d64f57af824d2ed5155a174d99b0de0027e3cc7f7a69110798"
-    sha256 cellar: :any_skip_relocation, ventura:      "482da35bc753cbdd956969b11a2ec3fe4bed9db69a8928e978f9aa2a34af11f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2f926dd87b5899ed49d40de33ff6bf53aebc248084d4962cf507665d8c65368f"
-  end
 
   depends_on "rust" => :build
 
