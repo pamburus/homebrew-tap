bac78636f3217db2dd586369ccf093869e2e8456
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-04-28 16:26:53 +0200

    new: updated hl to v0.28.0-beta.1
    Closes #5.
    
    Signed-off-by: Pavel Ivanov <12778560+pamburus@users.noreply.github.com>

--- Formula/hl.rb
+++ Formula/hl.rb
@@ -1,23 +1,16 @@
 class Hl < Formula
   desc "Log viewer for JSON and logfmt logs"
   homepage "https://github.com/pamburus/hl"
-  url "https://github.com/pamburus/hl/archive/refs/tags/v0.27.4.tar.gz"
-  sha256 "f1a6420d7fb0d049ddbd39e6844afe33c625f7a43816457219c4a8efad5c4c7c"
+  url "https://github.com/pamburus/hl/archive/refs/tags/v0.28.0-beta.1.tar.gz"
+  sha256 "46a420bcf62f5bdbf327e3b8345504595b46cb0747e9ae6d819f013d3cc90d34"
   license "MIT"
   head "https://github.com/pamburus/hl.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.27.4"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "35efd4cf60a3e02a49ab078645901510c3cc3a22424aa3af23c879520d313d0d"
-    sha256 cellar: :any_skip_relocation, ventura:      "3f0658a8f97fd3f8e9c8b3185c0f20284b3e72c88bf4c0aee08b99e441a811d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e0417cabf33d04d4554cd701811a25181333fc1e829b2b1355d406ce24867667"
-  end
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"hl", "--shell-completions")
   end
 
   test do
