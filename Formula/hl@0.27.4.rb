be6c9a71cea9b62cff83d5fec5e87c7e216cba0d
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-04-29 14:45:23 +0200

    new: added versioned hl formulas

---
+++ Formula/hl@0.27.4.rb
@@ -0,0 +1,26 @@
+class HlAT0274 < Formula
+  desc "Log viewer for JSON and logfmt logs"
+  homepage "https://github.com/pamburus/hl"
+  url "https://github.com/pamburus/hl/archive/refs/tags/v0.27.4.tar.gz"
+  sha256 "f1a6420d7fb0d049ddbd39e6844afe33c625f7a43816457219c4a8efad5c4c7c"
+  license "MIT"
+  head "https://github.com/pamburus/hl.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.27.4"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "35efd4cf60a3e02a49ab078645901510c3cc3a22424aa3af23c879520d313d0d"
+    sha256 cellar: :any_skip_relocation, ventura:      "3f0658a8f97fd3f8e9c8b3185c0f20284b3e72c88bf4c0aee08b99e441a811d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e0417cabf33d04d4554cd701811a25181333fc1e829b2b1355d406ce24867667"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "true"
+  end
+end
---
+++ Formula/hl@0.28.0.rb
@@ -0,0 +1,26 @@
+class HlAT0280 < Formula
+  desc "Log viewer for JSON and logfmt logs"
+  homepage "https://github.com/pamburus/hl"
+  url "https://github.com/pamburus/hl/archive/refs/tags/v0.28.0.tar.gz"
+  sha256 "63d6c96b0cf7e60514acec3a36d025a4e429dea6b0f8a2d7d1af0b9c87156ff9"
+  license "MIT"
+  head "https://github.com/pamburus/hl.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.28.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "54d3864d86b3284acee4c8b18022267e3dcbbb512b8690954131e6eb3adf7c26"
+    sha256 cellar: :any_skip_relocation, ventura:      "d3b81d68f2c02e09227039d178c98887de678f829d366b1b1706c82b99dbeb55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "69cb679f72fd65d338391740c0870929530cc2f1c6f129bfe994ac42aee5f4cb"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"hl", "--shell-completions")
+  end
+
+  test do
+    system "true"
+  end
+end
