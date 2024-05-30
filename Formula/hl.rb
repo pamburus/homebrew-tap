bbf5b5c85263392d297274db7f5f352f0575c6bd
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-05-30 21:57:29 +0200

    hl: update to 0.29.5
    Closes #22.
    
    Signed-off-by: Pavel Ivanov <12778560+pamburus@users.noreply.github.com>

--- Formula/hl.rb
+++ Formula/hl.rb
@@ -1,54 +1,52 @@
 class Hl < Formula
   desc "Log viewer for JSON and logfmt logs"
   homepage "https://github.com/pamburus/hl"
-  version "0.29.4"
+  version "0.29.5"
   license "MIT"
   head "https://github.com/pamburus/hl.git", branch: "master"
 
-  bottle do
-    root_url "https://github.com/pamburus/homebrew-tap/releases/download/hl-0.29.4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "040fec2219fa7b47dfde1ab0b6c02f77c763825e9d053d4ebd8590d79cdefa9e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "4feeb91746b76e54b589a4dad53267cdf3943ab59f6629d199f309ea9a397eb7"
-  end
-
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pamburus/hl/releases/download/v0.29.4/hl-macos-x86_64.tar.gz"
-      sha256 "230826871f5fae36769b5661ac20a9d5de4fddad98f54d585037f78244cf2f6b"
+      url "https://github.com/pamburus/hl/releases/download/v0.29.5/hl-macos-x86_64.tar.gz"
+      sha256 "588cd38071d6e66b6a04d69a5f43aea706e0802d73aba3c08a1aaba1d439001f"
 
       def install
         bin.install "hl"
         generate_completions_from_executable(bin/"hl", "--shell-completions")
+        (man1/"hl.1").write Utils.safe_popen_read(bin/"hl", "--man-page")
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/pamburus/hl/releases/download/v0.29.4/hl-macos-arm64.tar.gz"
-      sha256 "a674568138d144266a3f7b68117a9ff265ef8a0331fa889b98ded9ed1e5d82da"
+      url "https://github.com/pamburus/hl/releases/download/v0.29.5/hl-macos-arm64.tar.gz"
+      sha256 "a51fe10426489748d57d58f2e323b5cf91efeeca400f8a817bbc35837953b8d2"
 
       def install
         bin.install "hl"
         generate_completions_from_executable(bin/"hl", "--shell-completions")
+        (man1/"hl.1").write Utils.safe_popen_read(bin/"hl", "--man-page")
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pamburus/hl/releases/download/v0.29.4/hl-linux-x86_64-musl.tar.gz"
-      sha256 "6b8d7975fa7f47041298d1d609386f8ddec8a60727de453bf526669a86a90671"
+      url "https://github.com/pamburus/hl/releases/download/v0.29.5/hl-linux-x86_64-musl.tar.gz"
+      sha256 "ace202cad3c4cd7a80596427b06c059c6831610b8dd4a90ee9dc103dce38e8ae"
 
       def install
         bin.install "hl"
         generate_completions_from_executable(bin/"hl", "--shell-completions")
+        (man1/"hl.1").write Utils.safe_popen_read(bin/"hl", "--man-page")
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pamburus/hl/releases/download/v0.29.4/hl-linux-arm64-musl.tar.gz"
-      sha256 "6c6815d76e9ad80b5d3691bc45f571e1e49640e6e76058f32f425ee6bc1d19d3"
+      url "https://github.com/pamburus/hl/releases/download/v0.29.5/hl-linux-arm64-musl.tar.gz"
+      sha256 "a2f431275f72330bcfff03e165b6a789593dc9f11013a84770d304372ac60c1b"
 
       def install
         bin.install "hl"
         generate_completions_from_executable(bin/"hl", "--shell-completions")
+        (man1/"hl.1").write Utils.safe_popen_read(bin/"hl", "--man-page")
       end
     end
   end
