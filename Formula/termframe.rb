022b63b4ee63c3bbcf77c6a6ad04f0aaddff9725
Pavel Ivanov <12778560+pamburus@users.noreply.github.com>
2025-03-29 11:41:17 +0000

    termframe: add 0.4.2 bottle.

--- Formula/termframe.rb
+++ Formula/termframe.rb
@@ -5,6 +5,13 @@ class Termframe < Formula
   license "MIT"
   head "https://github.com/pamburus/termframe.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/pamburus/homebrew-tap/releases/download/termframe-0.4.2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d83e7985d633ce4278b31a7f1a11750ba2f3854fc5bbe1716b7eea3b785e8446"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4cc34fdf04a7a0306296c8ceb0fd9ab14097949deb1d0518c0b06c6751c31762"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce4c497dfc775eececacef551e2548ca35a6448008328a36aad3e328b0466668"
+  end
+
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/pamburus/termframe/releases/download/v0.4.2/termframe-macos-x86_64.tar.gz"
